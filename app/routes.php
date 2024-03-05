c99de66b3a5fab72602e251592f554000e2f2890
Argo Wibowo <argo.wibowo1989@gmail.com>
2024-03-05 15:40:59 +0700

    get and post obat

--- app/routes.php
+++ app/routes.php
@@ -10,6 +10,20 @@ use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
+
+    // Database configuration
+    $dsn = 'mysql:host=localhost;dbname=rumah_sakit;charset=utf8mb4';
+    $username = 'root';
+    $password = '';
+
+    // Set up PDO connection
+    try {
+        $pdo = new PDO($dsn, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
         // CORS Pre-Flight OPTIONS Request Handler
         return $response;
@@ -23,5 +37,38 @@ return function (App $app) {
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
+    });
+
+    //------------------------------------SILK-------------------------------------
+    $app->get('/obat', function (Request $request, Response $response) use ($pdo){
+        // $response->getBody()->write(json_encode(['foo' => 'bar']));
+
+        $stmt = $pdo->query('SELECT * FROM obat');
+        $obats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($obats));
+
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+    });
+
+    $app->post("/obat", function (Request $request, Response $response) use ($pdo){
+
+        $data = $request->getParsedBody();
+
+        $stmt = $pdo->prepare('INSERT INTO obat (sku, label_catatan, jumlah) VALUE (:sku, :label_catatan, :jumlah)');
+
+        $data = [
+            ":sku" => $data["sku"],
+            ":label_catatan" => $data["label_catatan"],
+            ":jumlah" => $data["jumlah"]
+        ];
+    
+        if($stmt->execute($data))
+        {
+            $response->getBody()->write(json_encode(['status' => 'berhasil']));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+        }
+        
+        $response->getBody()->write(json_encode(['status' => 'failed']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
     });
 };
